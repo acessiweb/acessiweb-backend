11754d0fd9b9462dce8229fcb66076efd75e56a2
lauravivan <lauvivang@gmail.com>
2025-05-14 18:33:29 -0300

    Add checker for username

--- src/common-users/entities/common-user.entity.ts
+++ src/common-users/entities/common-user.entity.ts
@@ -1,10 +1,11 @@
 import { User } from 'src/users/entities/user.entity';
 import { Project } from 'src/projects/entities/project.entity';
-import { ChildEntity, Column, OneToMany } from 'typeorm';
+import { Check, ChildEntity, Column, OneToMany } from 'typeorm';
 
 @ChildEntity('common')
+@Check(`"username" ~ '^[A-Za-z0-9]+$'`)
 export class CommonUser extends User {
-  @Column({ type: 'varchar', length: 50, nullable: false })
+  @Column({ type: 'varchar', length: 30, nullable: false })
   username: string;
 
   @OneToMany(() => Project, (project) => project.user)
