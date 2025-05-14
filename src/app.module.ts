f52bfc2a1c6e02d41a04c6629825d529f5eecb6d
lauravivan <lauvivang@gmail.com>
2025-05-14 18:49:38 -0300

    Update values to env variables and add common user projects module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { CommonUserModule } from './common-users/common-users.module';
-import { CommonUser } from './common-users/entities/common-user.entity';
-import { Guideline } from './guidelines/entities/guideline.entity';
-import { Project } from './projects/entities/project.entity';
-import { Deficiency } from './deficiences/entities/deficiences.entity';
-import { Auth } from './auth/entities/auth.entity';
 import { DeficiencesModule } from './deficiences/deficiences.module';
 import { GuidelinesModule } from './guidelines/guidelines.module';
-import { User } from './users/entities/user.entity';
 import { ProjectsModule } from './projects/projects.module';
+import { CommonUsersProjectsModule } from './common-users-projects/common-users-projects.module';
 
 @Module({
   imports: [
@@ -19,18 +14,19 @@ import { ProjectsModule } from './projects/projects.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 5432,
-      username: 'postgres',
+      username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: 'acessibiweb',
+      database: process.env.DB,
       synchronize: true, // true = shouldn't be used in production - otherwise you can lose production data
-      entities: [User, CommonUser, Guideline, Project, Deficiency, Auth],
+      autoLoadEntities: true,
     }),
     CommonUserModule,
     DeficiencesModule,
     GuidelinesModule,
     ProjectsModule,
+    CommonUsersProjectsModule,
   ],
 })
 export class AppModule {}
