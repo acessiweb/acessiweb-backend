95d7470616d004f891e601366093686445e44179
lauravivan <lauvivang@gmail.com>
2025-05-15 17:25:02 -0300

    Add columns for email and mobile phone verification

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -15,14 +15,47 @@ export class Auth {
   @Column({ type: 'varchar', length: 512, nullable: true })
   email: string;
 
-  @Column({ type: 'bytea', nullable: true })
+  @Column({ type: 'bytea', nullable: true, unique: true })
   emailHash: string;
+
+  @Column({ type: 'boolean', nullable: true, default: null })
+  isEmailVerified: boolean;
+
+  @Column({ type: 'varchar', nullable: true, unique: true })
+  verificationToken: string;
+
+  @Column({
+    type: 'timestamp without time zone',
+    nullable: true,
+  })
+  verificationTokenExpiry: Date;
+
+  @Column({ default: 0 })
+  emailVerificationAttempts: number;
 
   @Column({ type: 'varchar', length: 64, nullable: true })
   mobilePhone: string;
 
-  @Column({ type: 'bytea', nullable: true })
+  @Column({ type: 'bytea', nullable: true, unique: true })
   mobilePhoneHash: string;
+
+  @Column({ type: 'boolean', nullable: true, default: null })
+  isMobilePhoneVerified: boolean;
+
+  @Column({ type: 'varchar', nullable: true, unique: true })
+  smsVerificationCode: string;
+
+  @Column({
+    type: 'timestamp without time zone',
+    nullable: true,
+  })
+  smsVerificationCodeExpiry: Date;
+
+  @Column({ default: 0 })
+  smsVerificationAttempts: number;
+
+  @Column({ type: 'boolean', nullable: true, default: true })
+  active: boolean;
 
   @Column({ type: 'bytea' })
   password: string;
