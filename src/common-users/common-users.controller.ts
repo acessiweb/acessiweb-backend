e948335b9e79e3f6e25c88164dd2199bf3bbf830
lauravivan <lauvivang@gmail.com>
2025-05-15 22:12:20 -0300

    Add update

--- src/common-users/common-users.controller.ts
+++ src/common-users/common-users.controller.ts
@@ -6,11 +6,13 @@ import {
   Param,
   ParseUUIDPipe,
   Post,
+  Put,
 } from '@nestjs/common';
 import { CommonUserService } from './common-users.service';
 import { CreateCommonUserDto } from './dto/create-common-user.dto';
 import CustomException from 'src/common/exceptions/custom-exception.exception';
 import { throwHttpException } from 'src/common/errors/utils';
+import { updateCommonUserDto } from './dto/update-common-user.dto';
 
 @Controller('common-users')
 export class CommonUserController {
@@ -31,6 +33,20 @@ export class CommonUserController {
   async create(@Body() createCommonUserDto: CreateCommonUserDto) {
     try {
       return await this.commonUsersService.create(createCommonUserDto);
+    } catch (e) {
+      if (e instanceof CustomException) {
+        throwHttpException(e);
+      }
+    }
+  }
+
+  @Put(':cuid')
+  async update(
+    @Param('cuid', ParseUUIDPipe) cuid: string,
+    @Body() updateCommonUserDto: updateCommonUserDto,
+  ) {
+    try {
+      return await this.commonUsersService.update(cuid, updateCommonUserDto);
     } catch (e) {
       if (e instanceof CustomException) {
         throwHttpException(e);
