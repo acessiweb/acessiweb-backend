ed08bfc5775831d8862bfac26b9176ccfdc7fa80
lauravivan <lauvivang@gmail.com>
2025-05-27 20:22:32 -0300

    Remove common users projects folder

--- src/common-users-projects/dto/create-common-user-project.dto.ts
+++
@@ -1,6 +0,0 @@
-import { OmitType } from '@nestjs/mapped-types';
-import { CreateProjectDto } from 'src/projects/dto/create-project.dto';
-
-export class CreateCommonUserProjectDto extends OmitType(CreateProjectDto, [
-  'userId',
-] as const) {}
