b5a576c822116caac7534b62d2bdceb8c2985bba
lauravivan <lauvivang@gmail.com>
2025-05-27 20:21:43 -0300

    Add mock for deficiency service

---
+++ test/__mocks__/deficiences.service.mock.ts
@@ -0,0 +1,33 @@
+import { DeficiencesService } from 'src/deficiences/deficiences.service';
+import { Deficiency } from 'src/deficiences/entities/deficiences.entity';
+
+export const deficiencesMock = [
+  {
+    id: 'deficiencyId',
+    name: 'Visual',
+  } as Deficiency,
+  {
+    id: 'deficiency2Id',
+    name: 'Motora',
+  } as Deficiency,
+  {
+    id: 'deficiency3Id',
+    name: 'Cognitiva e Neural',
+  } as Deficiency,
+  {
+    id: 'deficiency4Id',
+    name: 'Auditiva',
+  } as Deficiency,
+  {
+    id: 'deficiency5Id',
+    name: 'TEA',
+  } as Deficiency,
+];
+
+export const deficiencesServiceMock = {
+  provide: DeficiencesService,
+  useValue: {
+    findOneBy: jest.fn().mockResolvedValue(deficiencesMock[0]),
+    findAll: jest.fn().mockResolvedValue(deficiencesMock),
+  },
+};
