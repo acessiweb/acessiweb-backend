404bcebb7395542a0e7a071c2370bb79dca873e9
lauravivan <lauvivang@gmail.com>
2025-08-06 23:12:55 -0300

    Fix filter types

--- src/types/filter.ts
+++ src/types/filter.ts
@@ -1,8 +1,22 @@
-export type FilterParams = {
+import { PaginationParams } from './pagination';
+
+export type FilterDefaultParams = {
   keyword?: string;
-  initialDate?: Date;
-  endDate?: Date;
+  initialDate?: string;
+  endDate?: string;
+};
+
+type GuidelineFilterDefault = FilterDefaultParams & {
   deficiences?: string[];
-  isDeleted?: boolean;
+};
+
+export type ProjectFilter = FilterDefaultParams & PaginationParams;
+
+export type GuidelineFilter = GuidelineFilterDefault & {
+  isDeleted?: boolean | string;
+};
+
+export type GuidelineRequestFilter = GuidelineFilterDefault & {
   statusCode?: string;
+  isRequest?: boolean | string;
 };
